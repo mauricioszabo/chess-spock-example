f79bda582830ad4b9a421994a9e34da0708e3756
Maurício Szabo <mauricio.szabo@gmail.com>
2021-07-06 22:51:34 -0300

    Undestanding chess notation

--- src/chess_spock_example/core.clj
+++ src/chess_spock_example/core.clj
@@ -100,7 +100,8 @@
                (= :piece (position (piece :other :_) :_ :_))
                (moves :piece (game :_ :board :_) [:k-row :k-col])])
     (move [:pos (game :_ :board :_) :result]
-          [(subtract :board [:pos] :without-me)
+          [(member :pos :board)
+           (subtract :board [:pos] :without-me)
            (not (= :board :without-me))
            (moves :pos (game :_ :board :_) [:new-row :new-col])
            (valid-pos [:new-row :new-col])
@@ -125,44 +126,106 @@
 
 (def rules (concat pawn-moves gen-moves rules-moves moves finish))
 
-
-#_
-(let [board [(piece 'white 'king 0 0)
-             ; (piece 'black 'rook 0 7)
-             ; (piece 'black 'rook 7 1)
-             (piece 'black 'rook 1 7)]]
-  (with-open [r (spock/with-rules rules)]
-    (map :new-board
-         (spock/solve {:rules r
-                       :bind {:piece (piece 'black 'rook 1 7)
-                              :combine combine
-                              :board board}}
-                      '(move :piece (game :_ :board :_) :new-board)))))
-                 ; '(finished white :board :ended))))
-
-
-#_
-(let [board [(piece 'white 'queen 4 3)
-             (piece 'black 'queen 4 5)
-             (piece 'white 'king 4 4)
-             (piece 'white 'bishop 5 4)
-             (piece 'white 'knight 6 4)]]
-  (with-open [r (spock/with-rules rules)]
-    (try
-      (cons board
-            (mapv :result
-                  (spock/solve {:rules r
-                                :bind {:board board
-                                       :pos (piece 'white 'king 4 4)}}
-                               '(move :pos (game :_ :board :_) :result)))))))
-
-
 (defn- piece [color kind row col]
   (list 'position
         (list 'piece color kind) row col))
 
+(def initial-board
+  (let [pieces ['[rook knight bishop queen king bishop knight rook] (repeat 8 'pawn)]
+        pieces (concat (map (fn [p] ['black p]) (apply concat pieces))
+                       (map (fn [p] ['white p]) (apply concat (reverse pieces))))
+        board (concat (for [row (range 2), col (range 8)] [row col])
+                      (for [row (range 6 8), col (range 8)] [row col]))]
+    (mapv (fn [[color kind] [row col]]
+            (piece color kind row col))
+          pieces
+          board)))
+
+(def pieces '{"K" king
+              "Q" queen
+              "R" rook
+              "B" bishop
+              "N" knight
+              "P" pawn})
+
+(defn- translate-row-col [rowcol]
+  (cond
+    (nil? rowcol) :_
+    (re-matches #"\d+" rowcol) (- 8 (Integer/parseInt rowcol))
+    :else (- (int (first rowcol)) 97)))
+
+(defn- chess-notation [string player]
+  (let [[beg end] (re-seq #"[KQRBN]?[a-h][1-8]" string)
+        [beg end] (if (nil? end)
+                    [nil beg]
+                    [beg end])
+        kind (pieces (re-find #"[KQRBN]" (or beg end)) 'pawn)
+        [_ start-col start-row] (some->> beg (re-find #"[KQRBN]?([a-h])?([1-8])?"))
+        [_ end-col end-row] (re-find #"[KQRBN]?([a-h])?([1-8])?" end)]
+    {:start (piece player
+                   kind
+                   (translate-row-col start-row)
+                   (translate-row-col start-col))
+     :end (piece player kind
+                 (translate-row-col end-row)
+                 (translate-row-col end-col))}))
+
+(defn- move-piece [rules board {:keys [start end]}]
+  (let [moves
+        (spock/solve {:rules rules :bind {:board board
+                                          :pos start
+                                          :end end}}
+                     '(and (move :pos (game :_ :board :_) :final-board)
+                           (member :end :final-board)))]
+    (when (-> moves count (not= 1))
+      (throw (ex-info "Not a valid move" {:possibilities (count moves)})))
+    (-> moves first :final-board)))
+
+(defn- play [rules {:keys [board player]} movement]
+  (let [notation (chess-notation movement player)
+        new-board (move-piece rules board notation)
+        new-player (if (= player 'black) 'white 'black)
+        [{:keys [finish]}] (spock/solve {:rules rules
+                                         :bind {:player new-player
+                                                :board new-board}}
+                                        '(finished :player :board :finish))]
+    (when finish
+      (throw (ex-info "GAME FINISHED" {:finish finish
+                                       :player new-player})))
+    {:board new-board
+     :player new-player}))
+
 #_
-(defn gen-chessboard []
+(with-open [r (spock/with-rules rules)]
+  (let [play (partial play r)]
+    (-> {:board initial-board :player 'white}
+        ; (play "e4")
+        ; (play "e5")
+        ; (play "Qf3")
+        ; (play "Na6")
+        ; (play "Bc4")
+        ; (play "Nb4")
+        ; (play "Qf7")
+        :board)))
+
+#_
+(def chess-example
+  [(piece 'black 'rook 0 3)
+   (piece 'black 'rook 0 7)
+   (piece 'white 'rook 3 0)
+   (piece 'white 'queen 4 4)
+   (piece 'white 'queen 4 7)
+   (piece 'rook 'queen 7 0)
+   (piece 'white 'queen 7 7)])
+
+#_
+(with-open [r (spock/with-rules rules)]
+  [chess-example
+   (move-piece r chess-example
+               (chess-notation "Qh4e1" 'white))])
+
+#_
+(defn gen-random-chessboard []
   (let [pieces (concat (repeat 8 'pawn)
                        '[bishop bishop queen king knight knight rook rook])
         pieces (for [piece pieces, color '[black white]] [color piece])
@@ -184,3 +247,18 @@ last-full-board
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+#_
+(let [board [(piece 'white 'queen 4 3)
+             (piece 'black 'queen 4 5)
+             (piece 'white 'king 4 4)
+             (piece 'white 'bishop 5 4)
+             (piece 'white 'knight 6 4)]]
+  (with-open [r (spock/with-rules rules)]
+    (try
+      (cons board
+            (mapv :result
+                  (spock/solve {:rules r
+                                :bind {:board board
+                                       :pos (piece 'white 'king 4 4)}}
+                               '(move :pos (game :_ :board :_) :result)))))))
